33b40f1b985a9c28a3bde98cabdffd4fd251654b
Dark Templar <xuyi556677@163.com>
2015-04-03 00:08:57 +0800

    get_location ok
    location info in wx.getLocation({..})

--- thu_schoolmap/public/scripts/9ed6173c.location.js
+++ thu_schoolmap/public/scripts/9ed6173c.location.js
@@ -2,7 +2,7 @@ $.post("/location",function(result){
     //console.log(result);
     //console.log("nice!");
     wx.config({
-        debug:true,
+        debug:false,
         appId:result.appId,
         timestamp:result.timestamp,
         nonceStr:result.nonceStr,
@@ -10,36 +10,32 @@ $.post("/location",function(result){
         jsApiList:result.jsApiList
     });
 
-    wx.checkJsApi({
-        jsApiList: ['getLocation'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-        success: function (res) {
-            // 以键值对的形式返回，可用的api值true，不可用为false
-            // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-
-        }
-    });
 });
-$.ready(function(){
+$(document).ready(function(){
     wx.ready(function(){
         wx.checkJsApi({
             jsApiList: [
                 'getLocation'
             ],
             success: function (res) {
-                alert(JSON.stringify(res));
+
             }
         });
         wx.getLocation({
             success: function (res) {
-                alert(JSON.stringify(res));
+                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                var speed = res.speed; // 速度，以米/每秒计
+                var accuracy = res.accuracy; // 位置精度
+
+                //alert(JSON.stringify(res));
             },
             cancel: function (res) {
-                alert('用户拒绝授权获取地理位置');
+                //alert('用户拒绝授权获取地理位置');
             }
         });
     });
     wx.error(function(res){
 
-        alert("1234");
     });
 });
