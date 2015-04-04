924d20056880c3cb4b205f57a9bc466ba3bd2a91
岳大禹 <yuedayu13@gmail.com>
2015-04-04 16:55:14 +0800

    实现准确定位并且写了一个简单的demo

--- thu_schoolmap/public/scripts/location.js
+++ thu_schoolmap/public/scripts/location.js
@@ -1,8 +1,13 @@
 var isDebug = true;
+var lastLocation = {
+    latitude: 0,
+    longitude: 0,
+    accuracy: 0
+};
+var locationLoop;
 
 $.post("/location", function (result) {
     isDebug = result.isDebug;
-    console.log(isDebug);
     if (!isDebug) {
         wx.config({
             debug: false,
@@ -12,17 +17,22 @@ $.post("/location", function (result) {
             signature: result.signature,
             jsApiList: result.jsApiList
         });
+        wx.ready(function() {
+            SetLocation();
+            locationLoop = setInterval("SetLocation()", 10000);
+        });
+    } else {
+        SetLocation();
+        locationLoop = setInterval("SetLocation()", 10000);
     }
-    SetLocation();
 });
 
 function Location(callback) {
     var location = {};
     if (isDebug) { //Debug mode, use browser location
-        var geolocation = new BMap.Geolocation();
-        geolocation.getCurrentPosition(function(r){
+        var geo = new BMap.Geolocation();
+        geo.getCurrentPosition(function(r){
             if(this.getStatus() == BMAP_STATUS_SUCCESS){
-                console.log(r.point);
                 location = {
                     latitude: r.point.lat,
                     longitude: r.point.lng,
@@ -36,27 +46,44 @@ function Location(callback) {
         wx.getLocation({
             success: function (res) {
                 var tempPoint = new BMap.Point(parseFloat(res.longitude), parseFloat(res.latitude));
-                alert(tempPoint.lng + " " + tempPoint.lat);
+                BMap.Convertor.translate(tempPoint, 0, function(point) {
+                    var accuracy = parseFloat(res.accuracy);
+                    if (accuracy >= 50)
+                    {
+                        if (lastLocation.longitude == 0) {
+                            location = {
+                                latitude: point.lat,
+                                longitude: point.lng,
+                                accuracy: accuracy
+                            };
+                        } else {
+                            location = lastLocation;
+                        }
+                    } else {
+                        //TODO: show path
+                        var polyline = new BMap.Polyline([
+                            lastLocation,
+                            location
+                        ], {strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5});   //创建折线
+                        map.addOverlay(polyline);
 
-                translateCallback = function(point) {
-                    alert("1");
-                    location = {
-                        latitude: point.lat,
-                        longitude: point.lng,
-                        accuracy: parseFloat(res.accuracy)
-                    };
+                        lastLocation = location;
+                        location = {
+                            latitude: point.lat,
+                            longitude: point.lng,
+                            accuracy: accuracy
+                        };
+                    }
                     callback(location);
-                };
-                setTimeout(function(){
-                    BMap.Convertor.translate(tempPoint, 2, translateCallback);
-                }, 1000);
-
+                });
             },
             cancel: function (res) {
+                clearInterval(locationLoop);
                 alert('用户拒绝授权获取地理位置');
             }
         });
         wx.error(function (res) {
+            clearInterval(locationLoop);
             alert("获取权限失败请重启应用");
         });
     }
