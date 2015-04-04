aba1e4d7ded571da8e636d37a2dedac1c3078e3f
MyXOF <xuyi556677@163.com>
2015-04-04 16:30:51 +0800

    search box

--- thu_schoolmap/public/scripts/UIcontrol.js
+++ thu_schoolmap/public/scripts/UIcontrol.js
@@ -1,7 +1,32 @@
 //console.log("UIcontrol.js!");
 
 $(document).ready(function(){
-    //        $("#navigation-div").fadeIn(2000,function(){});
-    //
+    $("#input-box").click(function(){
+        $("#clear-button").css("display","block");
 
+    });
+    $("#clear-button").click(function(){
+        //$("#search-content").attr("value","");
+        //$("#clear-button").css("display","none");
+        debugger;
+        alert("1");
+    });
+    $("#search-button").click(function(){
+        var cont = new BMap.Autocomplete(
+            {
+                "input": "search-content",
+                "location": map
+            });
+        map.clearOverlays();    //清除地图上所有覆盖物
+        function myFun() {
+            var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+            map.centerAndZoom(pp, 18);
+            map.addOverlay(new BMap.Marker(pp));    //添加标注
+        }
+
+        var local = new BMap.LocalSearch(map, { //智能搜索
+            onSearchComplete: myFun
+        });
+        local.search(myValue);
+    })
 });
