db1c5e338275a20698aa7fee5df52a20161ef00e
岳大禹 <yuedayu13@gmail.com>
2015-04-21 20:32:39 +0800

    好像解决了问题。求测试

--- thu_schoolmap/public/scripts/location.js
+++ thu_schoolmap/public/scripts/location.js
@@ -1,9 +1,6 @@
 var isDebug = true;
-var lastLocation_my = {
-    latitude: 0,
-    longitude: 0,
-    accuracy: 0
-};
+var isFirstTime = true;
+
 $.ajax({
     url: "location",
     type: "post",
@@ -43,7 +40,7 @@ function Location(callback) {
             accuracy: 0
         };
         $("#set-location").fadeIn();
-        lastLocation_my = location;
+        isFirstTime = false;
         callback(location);
     }
     var location = {};
@@ -52,56 +49,55 @@ function Location(callback) {
             var tempPoint = new BMap.Point(parseFloat(res.longitude), parseFloat(res.latitude));
             BMap.Convertor.translate(tempPoint, 0, function (point) {
                 var accuracy = parseFloat(res.accuracy);
-                alert("Hello" + lastLocation_my.latitude);
+                //alert("Hello" + isFirstTime + point.lat);
                 if (accuracy >= 50) {
-                    if (lastLocation_my.longitude == 0 && lastLocation_my.latitude == 0) {
+                    if (isFirstTime == true) {
                         if (accuracy <= 150) {
                             showModel("定位精度过低", "请确保打开GPS定位");
                             location = {
-                                latitude: point.lat,
-                                longitude: point.lng,
+                                latitude: parseFloat(point.lat),
+                                longitude: parseFloat(point.lng),
                                 accuracy: accuracy
                             };
-                            lastLocation_my.latitude = location.latitude;
-                            lastLocation_my.longitude = location.longitude;
-                            lastLocation_my.accuracy = location.accuracy;
+                            isFirstTime = false;
                             callback(location);
+                        } else {
+                            if (isFirstTime == true) {
+                                isFirstTime = false;
+                                showModel("定位失败", "定位精度过低，请手动定位。");
+                                $("#search-div").fadeOut();
+                                map.addEventListener("click", setLocationByHand);
+                                $("#set-location").click(function () {
+                                    $("#search-div").fadeIn();
+                                    $("#set-location").fadeOut();
+                                    map.removeEventListener("click", setLocationByHand);
+                                });
+                            }
                         }
-                        else {
-                            showModel("定位失败", "定位精度过低，请手动定位。");
-                            $("#search-div").fadeOut();
-                            map.addEventListener("click", setLocationByHand);
-                            $("#set-location").click(function () {
-                                $("#search-div").fadeIn();
-                                $("#set-location").fadeOut();
-                                map.removeEventListener("click", setLocationByHand);
-                            });
-                        }
-                    } else {
-                        callback(location);
                     }
                 } else {
                     location = {
-                        latitude: point.lat,
-                        longitude: point.lng,
+                        latitude: parseFloat(point.lat),
+                        longitude: parseFloat(point.lng),
                         accuracy: accuracy
                     };
-                    lastLocation_my.latitude = location.latitude;
-                    lastLocation_my.longitude = location.longitude;
-                    lastLocation_my.accuracy = location.accuracy;
+                    isFirstTime = false;
                     callback(location);
                 }
             });
         },
         fail:function(){
-            showModel("定位失败", "定位精度过低，请手动定位。");
-            $("#search-div").fadeOut();
-            map.addEventListener("click", setLocationByHand);
-            $("#set-location").click(function () {
-                $("#search-div").fadeIn();
-                $("#set-location").fadeOut();
-                map.removeEventListener("click", setLocationByHand);
-            });
+            if (isFirstTime) {
+                isFirstTime = false;
+                showModel("定位失败", "定位精度过低，请手动定位。");
+                $("#search-div").fadeOut();
+                map.addEventListener("click", setLocationByHand);
+                $("#set-location").click(function () {
+                    $("#search-div").fadeIn();
+                    $("#set-location").fadeOut();
+                    map.removeEventListener("click", setLocationByHand);
+                });
+            }
         },
         cancel: function (res) {
             alert('用户拒绝授权获取地理位置');
