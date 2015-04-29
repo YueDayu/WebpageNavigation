5eb0ef1ef407bc1bfe013d2d1af1e8b859e48a42
Dark Templar <xuyi556677@163.com>
2015-04-29 20:12:44 +0800

    add activity filter

--- thu_schoolmap/public/scripts/marker.js
+++ thu_schoolmap/public/scripts/marker.js
@@ -2,9 +2,22 @@ var myAcMarker = [];
 var Acsize=0;
 
 var roadcross;
-var roadblock_info;
 var activity_info;
+var roadblock_info;
 var scence_info;
+
+var indoor_act=[];
+var indoor_act_num=0;
+var outdoor_act=[];
+var outdoor_act_num=0;
+var school_match=[];
+var school_match_num=0;
+var school_exhibition=[];
+var school_exhibition_num=0;
+var school_meeting=[];
+var school_meeting_num=0;
+var currentMarker=[];
+var currentMarkrt_num=0;
 
 $(document).ready(function () {
     var isZoomed = true;
@@ -45,7 +58,7 @@ function addMarker(point, type,title,content){
             marker.openInfoWindow(infoWindow);
         });
     }
-    map.addOverlay(marker);
+//    map.addOverlay(marker);
     return marker;
 }
 
@@ -53,6 +66,7 @@ function addRoadBlock(){
     $.each(roadblock_info,function(infoIndex,info){
         var point = new BMap.Point(info["longitude"],info["latitude"]);
         var marker = addMarker(point,info["type"],info["title"],info["content"]);
+        map.addOverlay(marker);
         myAcMarker.push(marker);
         Acsize += 1;
     });
@@ -62,6 +76,7 @@ function addScence(){
     $.each(scence_info,function(infoIndex,info){
         var point = new BMap.Point(info["longitude"],info["latitude"]);
         var marker = addSMarker(point,info["type"],info["title"],info["content"]);
+        map.addOverlay(marker);
         myAcMarker.push(marker);
         Acsize += 1;
     });
@@ -83,8 +98,10 @@ function addSMarker(point,type, title,content) {
         var myIcon = new BMap.Icon("img/act.png", new BMap.Size(22,22));
     else if(type =="monument")
         var myIcon = new BMap.Icon("img/tower.png", new BMap.Size(12,23));
-    else
-        var myIcon = new BMap.Icon("img/scenic.png", new BMap.Size(12,12));
+    else if(type =="building")
+        var myIcon = new BMap.Icon("img/building.png", new BMap.Size(12,12));
+    else if(type =="scence")
+        var myIcon = new BMap.Icon("img/scence.png", new BMap.Size(20,20));
     var marker = new BMap.Marker(point, {
         icon: myIcon});
     if(title){
@@ -95,7 +112,7 @@ function addSMarker(point,type, title,content) {
             marker.openInfoWindow(infoWindow);
         });
     }
-    map.addOverlay(marker);
+    //map.addOverlay(marker);
     return marker;
 }
 
@@ -140,10 +157,38 @@ function getResourceNecessary(){
     });
     getResource("activity_info", function (data) {
         activity_info = data;
-        createMenuActivity(data);
+        filterActivity(activity_info);
+        createMenuActivity(activity_info);
     });
     getResource("scence_info",function(data){
         scence_info = data;
-        createMenuScence(data);
+        createMenuScence(scence_info);
+    });
+}
+
+function filterActivity(data){
+    $.each(data,function(infoIndex,info){
+        var point = new BMap.Point(info["longitude"],info["latitude"]);
+        var marker = addSMarker(point,info["type"],info["title"],info["content"]);
+        if(info["name"]=="indoor_act"){
+            indoor_act.push(marker);
+            indoor_act_num++;
+        }
+        else if(info["name"]=="outdoor_act"){
+            outdoor_act.push(marker);
+            outdoor_act_num++;
+        }
+        else if(info["name"]=="match"){
+            school_match.push(marker);
+            school_match_num++;
+        }
+        else if(info["name"]="exhibition"){
+            school_exhibition.push(marker);
+            school_exhibition_num++;
+        }
+        else if(info["name"]="meeting"){
+            school_meeting.push(marker);
+            school_meeting_num++;
+        }
     });
 }
